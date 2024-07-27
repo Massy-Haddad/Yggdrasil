a43ff52a02bb43a64fcdd3838b67571a86ffe44e
Massy Haddad <massy.haddad@lepisc.com>
2024-07-26 20:45:08 -0400

    Landing page - Navigation Header

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
+import { Header } from '@/components'
+
 interface MarketingLayoutProps {
 	children: React.ReactNode
 }
 
 const LandingPageLayout = ({ children }: MarketingLayoutProps) => {
-	return <main className="mx-auto flex-1 overflow-hidden">{children}</main>
+	return (
+		<>
+			<Header />
+			<main className="mx-auto flex-1 overflow-hidden">{children}</main>
+		</>
+	)
 }
 
 export default LandingPageLayout
